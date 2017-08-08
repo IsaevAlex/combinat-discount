d631408be021079119abb36018d954f5555337c9
Cronos <lexsandr46@gmail.com>
2017-08-08 13:47:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+$(document).ready(function(){
+    $("header").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
